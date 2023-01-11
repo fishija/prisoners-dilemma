42d8fda3ff0eb5df08b6af359fe07aa1018c2f0b
Jakub Mikula <cubamic@wp.pl>
2023-01-11 11:46:50 +0100

    test: test

--- ain_code/main.py
+++ ain_code/main.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     app = QApplication(sys.argv)
     # app_icon = QIcon('res/logger_icon.ico')
     # app.setWindowIcon(app_icon)
+    # test
 
     main_window = PDWindow()
     sys.exit(app.exec_())
